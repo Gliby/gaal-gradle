ea436c2d0a3e28a5e662cb7c3c7ff7a8c6efd1fb
Gliby <Gliby@users.noreply.github.com>
2019-10-25 00:23:59 +0300

    fixed invalid groovy

--- src/main/groovy/gliby/gaal/gradle/GAALPlugin.groovy
+++ src/main/groovy/gliby/gaal/gradle/GAALPlugin.groovy
@@ -3,10 +3,17 @@ package gliby.gaal.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+
 class GAALPlugin implements Plugin<Project> {
 
     @Override
-    void apply(Project target) {
+    void apply(Project project) {
         def extension = project.extensions.create('gaal', GAALObject)
+        // Add a task that uses configuration from the extension object
+        project.task('setupGaal') {
+            doLast {
+                println "working"
+            }
+        }
     }
 }
